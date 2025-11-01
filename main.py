bbda18d5fc7afd193f5275b2b8fae27d31c7d9d1
Alex <lxtak08@gmail.com>
2025-11-01 19:17:36 +0100

    connecting with kivy

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+#for iphone 13 configs
+from kivy.config import Config
+Config.set('graphics', 'width', '390')
+Config.set('graphics', 'height', '844')
+Config.set('graphics', 'resizable', False)
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
+
 from voiceRecognizer.functions import Voice_recognition
 from aI.functions import post_ai
 from textToSpeech.functions import text_to_speech
+
 from dotenv import load_dotenv
 import os
 
@@ -17,6 +28,8 @@ def main():
     print(ai_response)
     text_to_speech(ai_response)
     
+GUI = Builder.load_file("jarvis.kv")
     
-if __name__ == "__main__":
-    main()
+class Jarvis(App):
+    def build(self):
+        return GUI
---
+++ run.py
@@ -0,0 +1,4 @@
+from main import Jarvis
+
+if __name__ == "__main__":
+    Jarvis().run()
